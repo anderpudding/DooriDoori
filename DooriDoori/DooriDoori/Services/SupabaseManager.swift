72797067cc8d2ccec778aa94f3cc985d19969c6f
anderpudding <anderpuding0917@gmail.com>
2026-05-13 16:29:48 -0700

    Supabase client connected to IOS

---
+++ DooriDoori/DooriDoori/Services/SupabaseManager.swift
@@ -0,0 +1,15 @@
+import Foundation
+import Supabase
+
+final class SupabaseManager {
+    static let shared = SupabaseManager()
+
+    let client: SupabaseClient
+
+    private init() {
+        self.client = SupabaseClient(
+            supabaseURL: AppConfig.supabaseURL,
+            supabaseKey: AppConfig.supabaseAnonKey
+        )
+    }
+}
